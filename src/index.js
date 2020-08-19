e8e400bfea192ae94394f997c957ed70c0b5ce00
Francis Peckover <FrancisPeckover@users.noreply.github.com>
2020-08-19 22:28:11 +1000

    added colour randomiser to background animation

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-createStar(document.getElementById('starHolder'));
+var colours = [
+	'#00ffd9'
+];
+createStar(document.getElementById('starHolder'), colours);
 
-function createStar(starHolder) {
+function createStar(starHolder, colours) {
 	var star = document.createElement('div');
 	var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	let size = Math.floor(Math.random() * Math.floor(3)) + 1;
 	let pos = Math.floor(Math.random() * (w - size * 2) + size);
 	let animationDuration = size ** 0.5 * 20;
+	let colour = colours[Math.floor(Math.random() * colours.length)];
 	star.classList.add('star');
 	star.classList.add('center');
-	star.style.boxShadow = `${size}px ${size}px ${size}px ${size}px #ffffff`;
+	star.style.boxShadow = `${size}px ${size}px ${size}px ${size}px ${colour}`;
 	star.style.left = `${pos}px`;
 	star.style.animationDuration = `${animationDuration}s`;
 	starHolder.appendChild(star);
@@ -24,7 +28,7 @@ function createStar(starHolder) {
 	}, animationDuration * 1000);
 
 	setTimeout(() => {
-		createStar(starHolder);
+		createStar(starHolder, colours);
 	}, 0.5 * 1000);
 }
 
