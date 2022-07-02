804953e85e4f920073214d98937b276ca375a78a
Francis Peckover <peckover.francis@outlook.com>
2022-07-02 14:04:12 +1000

    working directory paths

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const Score = require('./models/score.model.js').ScoreModel;
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-const uri = 'mongodb+srv://Francis:M0nkeyman6797@cluster0.ibdt9.mongodb.net/database?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 8081;
+//const uri = 'mongodb+srv://Francis:M0nkeyman6797@cluster0.ibdt9.mongodb.net/database?retryWrites=true&w=majority';
 
 /* middleware */
 app.use(cors());
@@ -15,21 +15,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /* Connecting to the mongoose database */
+/*
 const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.connect(uri, mongoOptions).catch((err) => console.log(`Error: ${err}`));
 const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log('Successfully connected to mongoDB');
 });
+*/
 
 /* Server up files for the root directory */
 app.use('/', express.static(process.cwd() + '/../build'));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(process.cwd() + '../public/index.html'));
+	res.sendFile(path.resolve(process.cwd() + '/../public/index.html'));
 });
 
 /* Routing for CRUD of database */
+/*
 app.post('/scores', (req, res) => {
 	Score.countDocuments({ username: req.body.username })
 		.then((number) => {
@@ -78,6 +81,7 @@ app.delete('/scores/:id', (req, res) => {
 		.then(() => res.send(`Successfully deleted ${req.params.id} from database`))
 		.catch((err) => res.send(err));
 });
+*/
 
 /* Server listening */
 app.listen(PORT, () => {
